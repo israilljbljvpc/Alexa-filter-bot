13aa3cf43602c27fcaf1605bc2b5b9c7c8b2aeff
israilljbljvpc <isk613035@gmail.com>
2024-12-29 23:10:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Only_movies_sea
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001902019959').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "pathowtry")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://washeyare:pYyuC1yMJG98P7Tj@cluster0.3iw76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "washeyare")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
